1afe64b156df4734aed49b9332157f8cff75e75f
Romina Marsico <rominamarsico@users.noreply.github.com>
2019-01-22 21:08:11 +0100

    changed cloudfunction add message ref

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,7 @@ exports.addMessage = functions.https.onRequest((req, res) => {
   // Grab the text parameter.
   const original = req.query.text;
   // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  return admin.database().ref('/messages').set({original: original}).then((snapshot) => {
+  return admin.database().ref('/nfcTag').set({val: original}).then((snapshot) => {
     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
     return res.redirect(303, snapshot.ref.toString());
   });
